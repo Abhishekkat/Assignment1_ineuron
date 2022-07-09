b726f5532e999bc8bea4db18109fe066c964df9d
Abhishek katroliya <56072322+Abhishekkat@users.noreply.github.com>
2022-07-09 12:54:51 +0530

    Add files via upload

---
+++ main (6).cpp
@@ -0,0 +1,9 @@
+
+//6. WAP to print the name of the user in double quotes.
+#include<stdio.h>
+#include <string.h>
+int main(){
+    
+    printf("\"Abhishek\"");
+    return 0;
+}
---
+++ main (8).cpp
@@ -0,0 +1,12 @@
+
+//6. WAP to print the name of the user in double quotes.
+#include<stdio.h>
+#include <string.h>
+int main(){
+    
+    int x = printf("Ineuron");
+    printf("%d",x);
+    return 0;
+}
+
+// Output:Ineuron
